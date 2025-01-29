a1ff93132d34382d019177b23ce7013f093d72a1
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-29 16:23:08 +0300

    feat: create UserMapper to map User

---
+++ src/main/java/com/nebarrow/weathertracker/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.nebarrow.weathertracker.mapper;
+
+import com.nebarrow.weathertracker.dto.request.PostUser;
+import com.nebarrow.weathertracker.dto.response.GetUser;
+import com.nebarrow.weathertracker.model.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User toUser(PostUser postUserRequest);
+
+    GetUser toDto(User user);
+}
