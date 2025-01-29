8f6c9f0d2dca3b1f59b04e809348b9b30eab3135
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-29 22:28:10 +0300

    feat: create RegistrationController

---
+++ src/main/java/com/nebarrow/weathertracker/controller/RegistrationController.java
@@ -0,0 +1,38 @@
+package com.nebarrow.weathertracker.controller;
+
+import com.nebarrow.weathertracker.dto.request.PostUser;
+import com.nebarrow.weathertracker.exception.PasswordAreDifferentException;
+import com.nebarrow.weathertracker.model.User;
+import com.nebarrow.weathertracker.service.UserService;
+import com.nebarrow.weathertracker.util.HidePasswordUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/registration")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "sign-up-with-errors";
+    }
+
+    @PostMapping("/registration")
+    public void register(@ModelAttribute("user") User user, @RequestParam("repeatPassword") String repeatPassword) {
+        if (!user.getPassword().equals(repeatPassword)) {
+            log.error("Password should be identical {}, {}", user.getPassword(), repeatPassword);
+            throw new PasswordAreDifferentException("Passwords are different");
+        }
+            service.create(new PostUser(user.getLogin(), HidePasswordUtil.hashPassword(user.getPassword())));
+    }
+}
