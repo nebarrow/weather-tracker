fc041d7a86d72c27c695cb584643d83de0a5ef41
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-30 21:40:31 +0300

    feat: add finByLogin method

--- src/main/java/com/nebarrow/weathertracker/service/UserService.java
+++ src/main/java/com/nebarrow/weathertracker/service/UserService.java
@@ -2,11 +2,15 @@ package com.nebarrow.weathertracker.service;
 
 import com.nebarrow.weathertracker.dto.request.PostUser;
 import com.nebarrow.weathertracker.dto.response.GetUser;
+import com.nebarrow.weathertracker.exception.UserAlreadyExistsException;
 import com.nebarrow.weathertracker.exception.UserNotFoundException;
 import com.nebarrow.weathertracker.mapper.UserMapper;
 import com.nebarrow.weathertracker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,11 +21,18 @@ public class UserService {
     private UserMapper userMapper;
 
     public void create(PostUser postUser) {
-        userRepository.save(userMapper.toUser(postUser));
+        if (findByLogin(postUser.login()).isEmpty()) {
+            userRepository.save(userMapper.toUser(postUser));
+        } else {
+            throw new UserAlreadyExistsException("User with login " + postUser.login() + " already exists");
+        }
     }
 
-    public GetUser findById(int id) {
-        return userRepository.findById(id).map(userMapper::toDto)
-                .orElseThrow(() -> new UserNotFoundException("User with id not found"));
+    public Optional<GetUser> findById(int id) {
+        return userRepository.findById(id).map(userMapper::toDto);
+    }
+
+    public Optional<GetUser> findByLogin(String login) {
+        return userRepository.findByLogin(login).map(userMapper::toDto);
     }
 }
