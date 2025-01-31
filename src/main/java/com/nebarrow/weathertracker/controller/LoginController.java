7b4f7354119b1e5223a01af9007b4d9e08db9142
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-31 21:14:39 +0300

    refactor: delete unusable imports

--- src/main/java/com/nebarrow/weathertracker/controller/LoginController.java
+++ src/main/java/com/nebarrow/weathertracker/controller/LoginController.java
@@ -7,10 +7,11 @@ import com.nebarrow.weathertracker.service.UserService;
 import com.nebarrow.weathertracker.util.HidePasswordUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
