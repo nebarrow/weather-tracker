e7105fbabf7040eea07245600b07d47356bff57c
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-31 17:00:32 +0300

    feat: create LoginController with get and post methods

---
+++ src/main/java/com/nebarrow/weathertracker/controller/LoginController.java
@@ -0,0 +1,44 @@
+package com.nebarrow.weathertracker.controller;
+
+import com.nebarrow.weathertracker.exception.InvalidPasswordException;
+import com.nebarrow.weathertracker.exception.UserNotFoundException;
+import com.nebarrow.weathertracker.model.User;
+import com.nebarrow.weathertracker.service.UserService;
+import com.nebarrow.weathertracker.util.HidePasswordUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Slf4j
+@Controller
+public class LoginController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String showLoginForm(Model model) {
+        model.addAttribute("user", new User());
+        return "sign-in-with-errors";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute("user") User user) {
+        var foundUser = userService.findByLogin(user.getLogin());
+        if (foundUser.isEmpty()) {
+            log.error("User with login {} not found", user.getLogin());
+            throw new UserNotFoundException("User with login " + user.getLogin() + " not found");
+        }
+        var userDto = foundUser.get();
+        try {
+            HidePasswordUtil.checkPassword(user.getPassword(), userDto.password());
+        } catch (InvalidPasswordException e) {
+            log.error("Password is incorrect for user with login {}", user.getLogin());
+            throw new InvalidPasswordException("Password is incorrect");
+        }
+        return "redirect:/index";
+    }
+}
