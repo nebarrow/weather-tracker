6e5aeb8abcb039eb1b96495891113d975c868db1
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-01 19:27:03 +0300

    feat: add sessions

--- src/main/java/com/nebarrow/weathertracker/controller/AuthenticationController.java
+++ src/main/java/com/nebarrow/weathertracker/controller/AuthenticationController.java
@@ -3,10 +3,12 @@ package com.nebarrow.weathertracker.controller;
 import com.nebarrow.weathertracker.exception.InvalidPasswordException;
 import com.nebarrow.weathertracker.exception.UserNotFoundException;
 import com.nebarrow.weathertracker.model.User;
+import com.nebarrow.weathertracker.service.SessionService;
 import com.nebarrow.weathertracker.service.UserService;
+import com.nebarrow.weathertracker.util.CookieUtil;
 import com.nebarrow.weathertracker.util.HidePasswordUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,6 +23,9 @@ public class AuthenticationController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private SessionService sessionService;
+
     @GetMapping("/login")
     public String showLoginForm(Model model) {
         model.addAttribute("user", new User());
@@ -28,11 +33,14 @@ public class AuthenticationController {
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute User user) {
+    public String login(@ModelAttribute User user, HttpServletResponse response) {
         var foundUser = userService.findByLogin(user.getLogin())
                 .orElseThrow(() -> new UserNotFoundException("User not found"));
         try {
             HidePasswordUtil.checkPassword(user.getPassword(), foundUser.password());
+            var sessionId = sessionService.create(foundUser.id());
+            var cookie = CookieUtil.set(sessionId);
+            response.addCookie(cookie);
             return "redirect:/find";
         } catch (InvalidPasswordException e) {
             throw new InvalidPasswordException("Invalid password");
