2892a9d263a1748cfffd470fe30b2905269a045b
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-04 21:36:29 +0300

    feat: add method to search concrete cookie

--- src/main/java/com/nebarrow/weathertracker/util/CookieUtil.java
+++ src/main/java/com/nebarrow/weathertracker/util/CookieUtil.java
@@ -1,6 +1,7 @@
 package com.nebarrow.weathertracker.util;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.experimental.UtilityClass;
 
 import java.util.UUID;
@@ -11,6 +12,17 @@ public class CookieUtil {
     public static Cookie set(UUID id) {
         Cookie cookie = new Cookie("session_id", id.toString());
         cookie.setMaxAge(30 * 60);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
         return cookie;
+    }
+
+    public static boolean findByName(HttpServletRequest request, String name) {
+        for (Cookie cookie : request.getCookies()) {
+            if (cookie.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
