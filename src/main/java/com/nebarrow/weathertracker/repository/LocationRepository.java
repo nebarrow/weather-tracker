13e3bca5f44e0a6ec4798f67506512d80065c63b
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-04 21:43:51 +0300

    feat: add method to find user by id

--- src/main/java/com/nebarrow/weathertracker/repository/LocationRepository.java
+++ src/main/java/com/nebarrow/weathertracker/repository/LocationRepository.java
@@ -1,7 +1,11 @@
 package com.nebarrow.weathertracker.repository;
 
+import com.nebarrow.weathertracker.dto.response.LocationResponse;
 import com.nebarrow.weathertracker.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LocationRepository extends JpaRepository<Location, Integer> {
+    List<Location> findByUserId(int userId);
 }
