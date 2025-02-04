db522f7db0a7ee6a8a253518aad8cc1c2f32194f
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-04 21:38:20 +0300

    refactor: add ModelAndView return type instead ResponseEntity

--- src/main/java/com/nebarrow/weathertracker/controller/GlobalExceptionHandler.java
+++ src/main/java/com/nebarrow/weathertracker/controller/GlobalExceptionHandler.java
@@ -1,34 +1,55 @@
 package com.nebarrow.weathertracker.controller;
 
+import com.nebarrow.weathertracker.dto.request.RegistrationRequest;
 import com.nebarrow.weathertracker.exception.InvalidPasswordException;
+import com.nebarrow.weathertracker.exception.PasswordAreDifferentException;
 import com.nebarrow.weathertracker.exception.UserAlreadyExistsException;
 import com.nebarrow.weathertracker.exception.UserNotFoundException;
+import com.nebarrow.weathertracker.model.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
 @Slf4j
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleUserNotFound(UserNotFoundException e) {
+    public ModelAndView handleUserNotFound(UserNotFoundException e) {
         log.error("User not found", e);
-        return ResponseEntity.notFound().build();
+        ModelAndView mav = new ModelAndView("sign-in-with-errors");
+        mav.addObject("errorMessage", "Invalid username or password");
+        mav.addObject("user", new User());
+        return mav;
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
-    public ResponseEntity<String> handleInvalidPassword(InvalidPasswordException e) {
+    public ModelAndView handleInvalidPassword(InvalidPasswordException e) {
         log.error("Invalid password", e);
-        return ResponseEntity.badRequest().build();
+        ModelAndView mav = new ModelAndView("sign-in-with-errors");
+        mav.addObject("errorMessage", "Invalid username or password");
+        mav.addObject("user", new User());
+        return mav;
+    }
+
+    @ExceptionHandler(PasswordAreDifferentException.class)
+    public ModelAndView handlePasswordAreDifferent(PasswordAreDifferentException e) {
+        log.error("Passwords are different", e);
+        ModelAndView mav = new ModelAndView("sign-up-with-errors");
+        mav.addObject("errorMessage", "Passwords are different");
+        mav.addObject("registrationRequest", new RegistrationRequest("", "", ""));
+        return mav;
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<String> handleUserAlreadyExists(UserAlreadyExistsException e) {
+    public ModelAndView handleUserAlreadyExists(UserAlreadyExistsException e) {
         log.error("User already exists", e);
-        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        ModelAndView mav = new ModelAndView("sign-up-with-errors");
+        mav.addObject("errorMessage",
+                "Account with this email already exists. You can <a href='/login'>sign in here</a>.");
+        mav.addObject("registrationRequest", new RegistrationRequest("", "", ""));
+        return mav;
     }
-
-
 }
