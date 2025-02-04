147e59fa0d0a5caa71c1d81a774b003afee2126d
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-04 21:46:06 +0300

    feat: create method to delete old session from database

--- src/main/java/com/nebarrow/weathertracker/repository/SessionRepository.java
+++ src/main/java/com/nebarrow/weathertracker/repository/SessionRepository.java
@@ -1,9 +1,17 @@
 package com.nebarrow.weathertracker.repository;
 
 import com.nebarrow.weathertracker.model.Session;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface SessionRepository extends JpaRepository<Session, UUID> {
+    @Modifying
+    @Transactional
+    @Query("delete from Session s where s.expiresAt < current timestamp")
+    void deleteExpiredSessions();
 }
