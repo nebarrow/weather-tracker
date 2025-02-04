95fad22f6adacefffea824fa3fe8ad9a9ded3453
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-04 21:46:50 +0300

    feat: create method to delete old sessions at database

--- src/main/java/com/nebarrow/weathertracker/service/SessionService.java
+++ src/main/java/com/nebarrow/weathertracker/service/SessionService.java
@@ -1,14 +1,25 @@
 package com.nebarrow.weathertracker.service;
 
+import com.nebarrow.weathertracker.dto.response.SessionResponse;
+import com.nebarrow.weathertracker.mapper.SessionMapper;
 import com.nebarrow.weathertracker.model.Session;
 import com.nebarrow.weathertracker.repository.SessionRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
+
 
 @Service
 public class SessionService {
+
+    @Autowired
+    private SessionMapper sessionMapper;
+
     @Autowired
     private SessionRepository sessionRepository;
 
@@ -16,8 +27,19 @@ public class SessionService {
         var session = new Session();
         session.setId(UUID.randomUUID());
         session.setUserId(userId);
-        session.setExpiresAt(session.getExpiresAt().plusMinutes(20));
+        session.setExpiresAt(LocalDateTime.now().plusMinutes(30));
         sessionRepository.save(session);
         return session.getId();
+    }
+
+    public Optional<SessionResponse> findById(UUID id) {
+        return sessionRepository.findById(id)
+                .map(sessionMapper::toDto);
+    }
+
+    @Scheduled(fixedRate = 600000)
+    @Transactional
+    public void deleteExpiredSessions() {
+        sessionRepository.deleteExpiredSessions();
     }
 }
