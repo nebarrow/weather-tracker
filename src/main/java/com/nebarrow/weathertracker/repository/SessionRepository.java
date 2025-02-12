8d426b558254610ebf9b34db93a7b2b25ac4cbf5
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-12 11:26:14 +0300

    refactor: Add `@Repository` annotation

--- src/main/java/com/nebarrow/weathertracker/repository/SessionRepository.java
+++ src/main/java/com/nebarrow/weathertracker/repository/SessionRepository.java
@@ -1,14 +1,15 @@
 package com.nebarrow.weathertracker.repository;
 
-import com.nebarrow.weathertracker.model.Session;
+import com.nebarrow.weathertracker.model.entity.Session;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Repository
 public interface SessionRepository extends JpaRepository<Session, UUID> {
     @Modifying
     @Transactional
