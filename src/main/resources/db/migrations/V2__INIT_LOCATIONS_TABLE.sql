f6d6fcbf04af30b5535f665b6b6e1babf0e26365
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-12 14:38:08 +0300

    refactor: Update locations table schema

--- src/main/resources/db/migrations/V2__INIT_LOCATIONS_TABLE.sql
+++ src/main/resources/db/migrations/V2__INIT_LOCATIONS_TABLE.sql
@@ -2,6 +2,7 @@ CREATE TABLE Locations (
     ID SERIAL PRIMARY KEY,
     Name VARCHAR(128),
     UserId INTEGER NOT NULL REFERENCES Users(ID),
-    Latitude DECIMAL(4, 2),
-    Longtitude DECIMAL(4, 2)
+    Latitude DECIMAL(5, 2),
+    Longtitude DECIMAL(5, 2),
+    UNIQUE (UserId, Latitude, Longtitude)
 );
