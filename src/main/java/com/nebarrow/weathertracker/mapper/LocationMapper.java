df82d44fba0d9f19e61c86429544723217b501af
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-12 11:19:12 +0300

    feat: Add toEntity mapping

--- src/main/java/com/nebarrow/weathertracker/mapper/LocationMapper.java
+++ src/main/java/com/nebarrow/weathertracker/mapper/LocationMapper.java
@@ -1,10 +1,13 @@
 package com.nebarrow.weathertracker.mapper;
 
+import com.nebarrow.weathertracker.dto.request.LocationRequest;
 import com.nebarrow.weathertracker.dto.response.LocationResponse;
-import com.nebarrow.weathertracker.model.Location;
+import com.nebarrow.weathertracker.model.entity.Location;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface LocationMapper {
     LocationResponse toDto(Location location);
+
+    Location toEntity(LocationRequest weatherResponse);
 }
