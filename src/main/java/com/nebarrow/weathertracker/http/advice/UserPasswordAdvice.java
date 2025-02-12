19f0ba54b48c6e6b4029ec0404fa172fa7dd2b99
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-12 11:59:46 +0300

    refactor: Move password related methods to Advice

---
+++ src/main/java/com/nebarrow/weathertracker/http/advice/UserPasswordAdvice.java
@@ -0,0 +1,33 @@
+package com.nebarrow.weathertracker.http.advice;
+
+import com.nebarrow.weathertracker.dto.request.PostUser;
+import com.nebarrow.weathertracker.dto.request.RegistrationRequest;
+import com.nebarrow.weathertracker.exception.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@ControllerAdvice
+public class UserPasswordAdvice {
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    public ModelAndView handleInvalidPassword(InvalidPasswordException e) {
+        log.error("Invalid password {}", e.getMessage());
+        return createModelAndView("sign-in-with-errors", "Invalid login or password", new PostUser("", ""));
+    }
+
+    @ExceptionHandler(PasswordAreDifferentException.class)
+    public ModelAndView handlePasswordAreDifferent(PasswordAreDifferentException e) {
+        log.error("Passwords are different {}", e.getMessage());
+        return createModelAndView("sign-up-with-errors", "Passwords are different", new RegistrationRequest("", "", ""));
+    }
+
+    private ModelAndView createModelAndView(String viewName, String errorMessage, Object modelObject) {
+        ModelAndView mav = new ModelAndView(viewName);
+        mav.addObject("errorMessage", errorMessage);
+        mav.addObject(modelObject instanceof PostUser ? "postUser" : "registrationRequest", modelObject);
+        return mav;
+    }
+}
