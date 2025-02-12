1a25f2671c0152bac189ee9621d06eaf889df78f
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-12 11:20:08 +0300

    feat: Add findByUserIdAndLatitudeAndLongitude method

--- src/main/java/com/nebarrow/weathertracker/repository/LocationRepository.java
+++ src/main/java/com/nebarrow/weathertracker/repository/LocationRepository.java
@@ -1,11 +1,14 @@
 package com.nebarrow.weathertracker.repository;
 
-import com.nebarrow.weathertracker.dto.response.LocationResponse;
-import com.nebarrow.weathertracker.model.Location;
+import com.nebarrow.weathertracker.model.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface LocationRepository extends JpaRepository<Location, Integer> {
     List<Location> findByUserId(int userId);
+
+    Location findByUserIdAndLatitudeAndLongitude(int userId, double latitude, double longitude);
 }
