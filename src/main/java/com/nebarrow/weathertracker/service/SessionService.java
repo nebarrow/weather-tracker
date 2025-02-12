d315b3b4ba0de29ec73b9e553fe5c96be17d77a2
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-12 11:27:49 +0300

    feat: Add `@RequiredArgsConstructor`

--- src/main/java/com/nebarrow/weathertracker/service/SessionService.java
+++ src/main/java/com/nebarrow/weathertracker/service/SessionService.java
@@ -2,23 +2,24 @@ package com.nebarrow.weathertracker.service;
 
 import com.nebarrow.weathertracker.dto.response.SessionResponse;
 import com.nebarrow.weathertracker.mapper.SessionMapper;
-import com.nebarrow.weathertracker.model.Session;
+import com.nebarrow.weathertracker.model.entity.Session;
 import com.nebarrow.weathertracker.repository.SessionRepository;
+import com.nebarrow.weathertracker.util.CookieUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 import java.util.UUID;
 
-
 @Service
+@RequiredArgsConstructor
 public class SessionService {
 
-    @Autowired
-    private SessionMapper sessionMapper;
+    private final SessionMapper sessionMapper;
+    private final SessionRepository sessionRepository;
 
     @Autowired
     private SessionRepository sessionRepository;
@@ -39,6 +40,16 @@ public class SessionService {
 
     public void delete(UUID id) {
         sessionRepository.deleteById(id);
+    }
+
+    public void createSessionAndAddCookie(UUID sessionId, HttpServletResponse response) {
+        var cookie = CookieUtil.set(sessionId);
+        response.addCookie(cookie);
+    }
+
+    public void deleteSessionCookie(String sessionId, HttpServletResponse response) {
+        var cookie = CookieUtil.delete(UUID.fromString(sessionId));
+        response.addCookie(cookie);
     }
 
     @Scheduled(fixedRate = 600000)
