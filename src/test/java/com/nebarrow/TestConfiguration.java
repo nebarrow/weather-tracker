cecf98677a3317584ba452660537a583b17eca06
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-15 11:34:42 +0300

    feat: Add test configuration

---
+++ src/test/java/com/nebarrow/TestConfiguration.java
@@ -0,0 +1,110 @@
+package com.nebarrow;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.util.Properties;
+
+@Configuration
+@Profile("application-test")
+@ComponentScan("com.nebarrow.weathertracker")
+@EnableWebMvc
+@EnableJpaRepositories("com.nebarrow.weathertracker.repository")
+@EnableTransactionManagement
+@PropertySource("classpath:application-test.properties")
+@EnableScheduling
+public class TestConfiguration implements WebMvcConfigurer {
+
+    @Value("${db.url}")
+    private String dbUrl;
+
+    @Value("${db.username}")
+    private String dbUser;
+
+    @Value("${db.driver}")
+    private String dbDriver;
+
+    @Value("${db.password}")
+    private String dbPassword;
+
+    @Value("${openweather.api}")
+    private String apiKey;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(dbDriver);
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUser);
+        dataSource.setPassword(dbPassword);
+
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        properties.put("hibernate.hbm2ddl.auto", "none");
+        return properties;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource());
+        emf.setPackagesToScan("com.nebarrow.weathertracker.model.entity");
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        emf.setJpaProperties(hibernateProperties());
+        return emf;
+    }
+
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager platformTransactionManager() {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return jpaTransactionManager;
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("https://api.openweathermap.org/")
+                .filter(((request, next) -> {
+                    URI uri = UriComponentsBuilder.fromUri(request.url())
+                            .queryParam("appid", apiKey)
+                            .build()
+                            .toUri();
+                    ClientRequest newRequest = ClientRequest.from(request).url(uri).build();
+                    return next.exchange(newRequest);
+                }))
+                .build();
+    }
+
+    @Bean
+    public Flyway flyway() {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource())
+                .locations("classpath:db/migration")
+                .load();
+        flyway.migrate();
+        return flyway;
+    }
+}
