6b2df7db6c22490da8eefc25fbd7cd3732492c3f
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-02-16 16:56:40 +0300

    feat: Update location precision and enhance tests

--- src/main/resources/db/migrations/V2__INIT_LOCATIONS_TABLE.sql
+++ src/main/resources/db/migrations/V2__INIT_LOCATIONS_TABLE.sql
@@ -2,7 +2,7 @@ CREATE TABLE Locations (
     ID SERIAL PRIMARY KEY,
     Name VARCHAR(128),
     UserId INTEGER NOT NULL REFERENCES Users(ID),
-    Latitude DECIMAL(5, 2),
-    Longtitude DECIMAL(5, 2),
+    Latitude DECIMAL(6, 2),
+    Longtitude DECIMAL(6, 2),
     UNIQUE (UserId, Latitude, Longtitude)
 );
